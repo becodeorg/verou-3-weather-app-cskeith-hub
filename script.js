374a05c08a0f9cc7a5361568c7d0844065996d88
Rustiek <cskeith01@gmail.com>
2022-01-24 21:23:57 +0100

    Add Start for Card loop

--- script.js
+++ script.js
@@ -15,16 +15,37 @@ submit.addEventListener('click', function () {
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      card(data);
+      card1(data);
+      card2(data);
+      createCard(data);
     })
 
 
 })
 
-const card = (data) => {
-  temp.innerHTML = data.list[0].main.temp;
+const card1 = (data) => {
+  temp.innerHTML = data.list[0].main.temp + "<span>&#8451;</span>";
   date.innerHTML = data.list[0].dt_txt;
-  wind.innerHTML = data.list[0].wind.speed;
-
-  console.log(data.list[0].dt_txt);
+  wind.innerHTML = data.list[0].wind.speed + "<span> Meters per Second</span>";
 }
+
+const card2 = (data) => {
+  temp.innerHTML = data.list[1].main.temp + "<span>&#8451;</span>";
+  date.innerHTML = data.list[1].dt_txt;
+  wind.innerHTML = data.list[1].wind.speed + "<span> Meters per Second</span>";
+}
+
+
+// TODO  loop for html card
+const createCard = (data) => {
+  for (var i = 0; i < data.length; i++)
+  const section = document.createElement("section");
+  const day = document.createElement("div");
+  const h1 = document.createElement("h1");
+  const h3 = document.createElement("h3");
+  const datum = document.createElement("div");
+  const paragraph = document.createElement("p");
+  const temperature = document.createElement("div");
+  const wd = document.createElement("div");
+}
+createCard();
