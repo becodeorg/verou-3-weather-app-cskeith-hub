2c39d95dc9b66f9909cef7964344ccc186958052
Rustiek <cskeith01@gmail.com>
2022-01-31 16:50:16 +0100

    Looking to optimize more

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 import apiKey from "./config.js";
 
-const submit = document.getElementById("submit");
-const weatherPLace = document.getElementById("weatherPlace");
-
 const addEveryWeekDay = () => {
   const weekDay = ["Sunday",
     "Monday",
@@ -32,6 +29,8 @@ const temperature = (tempData) => {
   return everyDayTemperature;
 }
 
+const submit = document.getElementById("submit");
+const weatherPLace = document.getElementById("weatherPlace");
 
 submit.addEventListener('click', function () {
   let place = weatherPLace.value;
@@ -43,7 +42,7 @@ submit.addEventListener('click', function () {
         .then(response => response.json())
         .then(data => {
           const dailyTemperature = temperature(data);
-          chart(data, dayLabel, dailyTemperature);
+          chart(dayLabel, dailyTemperature);
           cardInfo(data, image);
           addCard(data.list[0], "card", dayLabel[0]);
           addCard(data.list[8], "card", dayLabel[1]);
@@ -53,7 +52,27 @@ submit.addEventListener('click', function () {
         })
     })
 
-  const chart = (data, dayLabel,dailyTemperature) => {
+  const createDivElement = () => {
+    const newDiv = document.createElement("div");
+    return newDiv;
+  }
+
+  const createParagraphElement = () => {
+    const newParagraph = document.createElement("p");
+    return newParagraph;
+  }
+
+  const createH1Element = () => {
+    const newH1 = document.createElement("h1");
+    return newH1;
+  }
+
+  const createH3Element = () => {
+    const newH3 = document.createElement("h3");
+    return newH3;
+  }
+
+  const chart = (dayLabel, dailyTemperature) => {
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, {
       type: 'bar',
@@ -61,7 +80,7 @@ submit.addEventListener('click', function () {
         labels: dayLabel,
         datasets: [{
           label: 'Temperature',
-          data: [dailyTemperature[0], dailyTemperature[1], dailyTemperature[2], dailyTemperature[3], dailyTemperature[4],],
+          data: [dailyTemperature[0], dailyTemperature[1], dailyTemperature[2], dailyTemperature[3], dailyTemperature[4], ],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(255, 99, 132, 0.2)',
@@ -90,128 +109,80 @@ submit.addEventListener('click', function () {
     });
   }
 
-  const createDivElement = () =>{
-    const newDiv = document.createElement("div");
-    return newDiv;
-  }
-
-  const createParagraphElement = () =>{
-    const newParagraph = document.createElement("p");
-    return newParagraph;
-  }
-
   const cardInfo = (data, image) => {
     const main = document.querySelector("main");
     const section = document.createElement("section");
     main.appendChild(section);
-    
     const infoCard = createDivElement();
     infoCard.className = "infoCard";
     section.appendChild(infoCard);
-
-    const locationH1 = document.createElement("h1");
+    const locationH1 = createH1Element();
     locationH1.innerText = data.city.name;
     infoCard.appendChild(locationH1);
     const city = createParagraphElement();
     city.innerText = data.city.country;
     infoCard.appendChild(city);
-
     const cityImage = createDivElement();
     cityImage.className = "cityImage";
     cityImage.innerHTML = `<img src=${image.results[0].urls.regular} alt="City of" style="width:200px;height:200px">`;
     infoCard.appendChild(cityImage);
-
   }
 
   const addCard = (data, style, addEveryWeekDay, hoverEffects) => {
     const main = document.querySelector("main");
     const section = document.createElement("section");
     main.appendChild(section);
-
-    function hoverEffects(cardSelected) {
-      cardSelected.onmouseover = function () {
-        cardSelected.style.transform = "scale(1.03, 1.03)";
-        cardSelected.style.zIndex = 100;
-        cardSelected.style.transition = "0.15s";
-        cardSelected.style.boxShadow = "0 0 0 9999px #000000b0";
-      };
-      cardSelected.onmouseout = function () {
-        cardSelected.style.transform = "scale(1, 1)";
-        cardSelected.style.zIndex = 0;
-        cardSelected.style.boxShadow = "0 0 0 9999px #00000000";
-      };
-    }
-    hoverEffects(section);
-
-
     const day1 = createDivElement();
     day1.className = style;
     section.appendChild(day1);
-
-    const mondayH1 = document.createElement("h1");
+    const mondayH1 = createH1Element();
     mondayH1.innerText = addEveryWeekDay;
     day1.appendChild(mondayH1);
-
     const dateParagraph = createParagraphElement();
     day1.appendChild(dateParagraph);
     dateParagraph.innerHTML = data.dt_txt;
-
-    const tempH3 = document.createElement("h3");
+    const tempH3 = createH3Element();
     tempH3.innerText = "Temperature:";
     day1.appendChild(tempH3);
-
     const temperature = createDivElement();
     temperature.className = "temperature";
     day1.append(temperature);
-
     const tempParagraph = createParagraphElement();
     day1.append(tempParagraph);
     tempParagraph.innerHTML = data.main.temp + "<span>&#8451;</span>";
-
-    const feelTempH3 = document.createElement("h3");
+    const feelTempH3 = createH3Element();
     feelTempH3.innerText = "Feeling Temperature:";
     day1.appendChild(feelTempH3);
-
     const feelTemp = createDivElement();
     feelTemp.className = "feelTemp";
     day1.append(feelTemp);
-
     const feelTempParagraph = createParagraphElement();
     day1.append(feelTempParagraph);
     feelTempParagraph.innerHTML = data.main.feels_like + "<span>&#8451;</span>";
-
-    const weatherH3 = document.createElement("h3");
+    const weatherH3 = createH3Element();
     weatherH3.innerText = "Weather:";
     day1.append(weatherH3);
-
     const weather = createDivElement();
     weather.className = "weather";
     day1.append(weather);
-
     const weatherParagraph = createParagraphElement();
     weatherParagraph.innerHTML = data.weather[0].main;
     day1.append(weatherParagraph);
-
-    const windH3 = document.createElement("h3");
+    const windH3 = createH3Element();
     windH3.innerText = "Wind:";
     day1.append(windH3);
-
     const wind = createDivElement();
     wind.className = "wind";
     day1.append(wind);
-
     const windParagraph = createParagraphElement();
     windParagraph.innerHTML = data.wind.speed + "<span>MpS</span>";
     day1.append(windParagraph);
-
-    const humidityH3 = document.createElement("h3");
+    const humidityH3 = createH3Element();
     humidityH3.innerText = "Humidity:";
     day1.append(humidityH3);
-
     const humidity = createDivElement();
     humidity.className = "humidity";
     day1.append(humidity);
-
     const humidityParagraph = createParagraphElement();
     humidityParagraph.innerHTML = data.main.humidity + "<span>%</span>";
     day1.append(humidityParagraph);
@@ -219,4 +190,3 @@ submit.addEventListener('click', function () {
 
 
 })
-
